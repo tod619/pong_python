34a1c8423ff72c93ee717ad65f90c88edf3e45e6
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2023-06-21 19:22:19 +0100

    Fix bug that started the paddle in center in main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
 # Create the paddle
 paddle = Turtle()
@@ -31,6 +32,12 @@ def go_down():
 screen.listen()
 screen.onkey(go_up, "Up")
 screen.onkey(go_down, "Down")
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+
 
 # Exit the screen on click
 screen.exitonclick()
