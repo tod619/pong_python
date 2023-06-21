46149e88bf62d5e2c7e9707ba0ff1c6284fe22c0
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2023-06-21 19:09:03 +0100

    Set up the screen with width, height, color, + title in main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # A game of Pong
 # Created using the turtle graphics library in python
 # 21/06/2023
+from turtle import Screen
+
+# Set up the screen
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+
+# Exit the screen on click
+screen.exitonclick()
