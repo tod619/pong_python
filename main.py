0d21ecb9c09b8abdedb49b3dda35ec6a33baed77
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2023-06-21 19:19:07 +0100

    Create paddle in main.py file

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 # A game of Pong
 # Created using the turtle graphics library in python
 # 21/06/2023
-from turtle import Screen
+from turtle import Screen, Turtle
 
 # Set up the screen
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+
+# Create the paddle
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.penup()
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.goto(350, 0)
+
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
 
 # Exit the screen on click
 screen.exitonclick()
