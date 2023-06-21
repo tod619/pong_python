cf6ed91b96d423aedc9b6edb03ffd1438e7007fd
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2023-06-21 20:02:59 +0100

    Refactor code,create paddle class, move paddle code into paddle.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Created using the turtle graphics library in python
 # 21/06/2023
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 # Set up the screen
 screen = Screen()
@@ -11,27 +12,23 @@ screen.title("Pong")
 screen.tracer(0)
 
 # Create the paddle
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.penup()
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.goto(350, 0)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
+# def go_up():
+#     new_y = paddle.ycor() + 20
+#     paddle.goto(paddle.xcor(), new_y)
 
 
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
+# def go_down():
+#     new_y = paddle.ycor() - 20
+#     paddle.goto(paddle.xcor(), new_y)
 
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+# screen.onkey(go_up, "Up")
+# screen.onkey(go_down, "Down")
 
 game_is_on = True
 
---
+++ paddle.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    """ A paddle class that inherits from Trurtl """
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(position)
